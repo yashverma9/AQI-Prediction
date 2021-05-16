ff4211c79ec2250a2e3314082bd8b1746a1f14f7
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2021-05-16 23:38:48 +0530

    Added api for chart

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,6 +4,7 @@ import sys
 import os
 from flask_cors import CORS, cross_origin
 
+import requests
 import pandas as pd
 import pickle
 
@@ -22,11 +23,28 @@ def predict_aqi(data: dict, filename: str) -> float:
     return model.predict(df)[0]
 
 
+def getRecentJson():
+    df = pd.read_csv("data/AQI/recent.csv")
+    date = df['date'].tolist()
+    aqi = df['PM2.5'].tolist()
+    dict = {"date": date , "aqi2.5" : aqi} 
+    return dict
+
 
 @app.route('/')
 @cross_origin()
 def getHello():
     return "Hello! This is running!"
+
+
+@app.route('/city')
+@cross_origin()
+def get_city_data() -> dict:
+    city = request.args['city']
+    api_key = '4b3b3ba35fcd7a3d24f3adc38895bbdd' #Fill here
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={api_key}&units=metric'
+    return jsonify(requests.get(url).json())
+
 
 
 @app.route('/getPrediction', methods = ['POST'])
@@ -43,6 +61,20 @@ def getPrediction():
 
 
 
+@app.route('/aqiMonthChart') #Also for weekly
+def aqiMonth():
+    return flask.jsonify(getRecentJson())
+
+
+
+@app.route('/tempMonthChart')
+def tempMonth():
+    return
+
+
+@app.route('/humidityMonthChart')
+def humMonth():
+    return
 
 if __name__ == "__main__":
-    app.run(debug=True, port = 7000)
+    app.run(debug=True, port = 5000)
